a791014d619baded6af150bb105841463b2ccffe
Piotr Komorowski <pjpkomorowski@gmail.com>
2018-05-29 18:06:36 +0200

    Some refactoring and documentation

--- src/main/scala/com/jps/wbtree/WBTreeSet.scala
+++ src/main/scala/com/jps/wbtree/WBTreeSet.scala
@@ -6,9 +6,34 @@ import scala.annotation.tailrec
 import scala.collection.{mutable}
 
 object DataOrdering {
+
+  /**
+    * Define comparing function for a type
+    * @tparam T type of data stored in WBTreeSet
+    */
   trait MyOrdering[T] {
+    /**
+      * Check if a value is less than b.
+      * @param a first value
+      * @param b second value
+      * @return true if a value is less than b otherwise false
+      */
     def lt(a: T, b: T): Boolean
+
+    /**
+      * Check if a value is equal to b.
+      * @param a first value
+      * @param b second value
+      * @return true if a value is equal to b otherwise false
+      */
     def eq(a: T, b: T): Boolean
+
+    /**
+      * Check if a value is greater than b.
+      * @param a first value
+      * @param b second value
+      * @return true if a value is greater than b otherwise false
+      */
     def gt(a: T, b: T): Boolean
   }
   implicit object DoubleOrdering extends MyOrdering[Double] {
@@ -34,24 +59,112 @@ object DataOrdering {
 }
 
 sealed abstract class WBTreeSet[+A: MyOrdering] {
+  /**
+    * Get number of data entries in WBTreeSet
+    * @return number of values in WBTreeSet
+    */
   def size: Int = 0
+
+  /**
+    * Get WBTreeSet iterator.
+    * @tparam T type of data stored in WBTreeSet
+    * @return WBTreeIterator at the beginning of WBTreeSet
+    */
   def iterator[T >: A : MyOrdering]: Iterator[T] = Iterator.empty
+
+  /**
+    * Add value to WBTreeSet.
+    * @param valueAdded value which will be inserted
+    * @tparam T type of a inserted value
+    * @return copy of this WBTreeSet with the value inserted
+    */
   def add[T >: A : MyOrdering](valueAdded: T): WBTreeSet[T] = Node(valueAdded, WBTreeSetNil, WBTreeSetNil)
+
+  /**
+    * Check if value exists in WBTreeSet.
+    * @param value checked value
+    * @tparam T type of a checked value
+    * @return true if value is in a WBTreeSet otherwise false
+    */
   def exists[T >: A : MyOrdering](value: T): Boolean = false
+
+  /**
+    * Intersect two WBTreeSets.
+    * @param other WBTreeSet to intersect with
+    * @tparam T type of data stored in WBTreeSet
+    * @return intersection of two WBTreeSets
+    */
   def intersect[T >: A : MyOrdering](other: WBTreeSet[T]): WBTreeSet[T] = WBTreeSetNil
+
+  /**
+    * Sum two WBTreeSets.
+    * @param other WBTreeSet to sum with
+    * @tparam T type of data stored in WBTreeSet
+    * @return sum of two WBTreeSet
+    */
   def sum[T >: A : MyOrdering](other: WBTreeSet[T]): WBTreeSet[T] = other
+
+  /**
+    * Remove value from WBTreeSet.
+    * @param valueRemoved value which will be removed
+    * @tparam T type of a removed value
+    * @return copy of this WBTree with the value removed
+    */
   def remove[T >: A : MyOrdering](valueRemoved: T): WBTreeSet[T] = throw new NoSuchElementException(String.valueOf(valueRemoved))
+
+  /**
+    * Reabalance WBTreeSet.
+    * @tparam T type of a inserted value
+    * @return rebalanced WBTreeSet
+    */
   def rebalance[T >: A : MyOrdering]: WBTreeSet[T] = this
+
+  /**
+    *
+    * @tparam T type of data stored in WBTreeSet
+    * @return
+    */
   def remove_and_get_min[T >: A : MyOrdering]: (T, WBTreeSet[T]) = throw new IllegalStateException()
+
+  /**
+    * Do single left rotation on WBTreeSet.
+    * @tparam T type of data stored in WBTreeSet
+    * @return WBTreeSet after single  left rotation
+    */
   def singleLeftRotate[T >: A : MyOrdering]: Node[T] = throw new IllegalStateException()
+
+  /**
+    * Do double left rotation on WBTreeSet.
+    * @tparam T type of data stored in WBTreeSet
+    * @return WBTreeSet after double left rotation
+    */
   def doubleLeftRotate[T >: A : MyOrdering]: Node[T] = throw new IllegalStateException()
+
+  /**
+    * Do single right rotation on WBTreeSet.
+    * @tparam T type of data stored in WBTreeSet
+    * @return WBTreeSet after single right rotation
+    */
   def singleRightRotate[T >: A : MyOrdering]: Node[T] = throw new IllegalStateException()
+
+  /**
+    * Do double right rotation on WBTreeSet.
+    * @tparam T type of data stored in WBTreeSet
+    * @return WBTreeSet after double right rotation
+    */
   def doubleRightRotate[T >: A : MyOrdering]: Node[T] = throw new IllegalStateException()
 }
 
 case object WBTreeSetNil extends WBTreeSet[Nothing] {
 }
 
+/**
+  * Is a subclass of WBTreeSet
+  * @param value current value in node
+  * @param left left node
+  * @param right right node
+  * @tparam A type of data stored in WBTreeSet
+  */
 case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]) extends WBTreeSet[A] {
 
   val delta: Double = 1 + Math.sqrt(2)
@@ -95,30 +208,35 @@ case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]
     else
       this
   }
+
   override def singleLeftRotate[T >: A : MyOrdering]: Node[T] = {
     this match {
       case Node(curVal, curLeft, Node(curRightVal, curRightLeft, curRightRight))
         => Node(curRightVal, Node(curVal, curLeft, curRightLeft), curRightRight)
         }
   }
+
   override def doubleLeftRotate[T >: A : MyOrdering]: Node[T] = {
     this match {
       case Node(curVal, curLeft, Node(rightVal, Node(rightLeftVal, rightLeftLeft, rightLeftRight), rightRight))
-        => Node[T](rightLeftVal, Node(curVal, curLeft, rightLeftLeft), Node(rightVal, rightLeftRight, rightRight))
+        => Node(rightLeftVal, Node(curVal, curLeft, rightLeftLeft), Node(rightVal, rightLeftRight, rightRight))
     }
   }
+
   override def singleRightRotate[T >: A : MyOrdering]: Node[T] = {
     this match {
       case Node(curVal, Node(curLeftVal, curLeftLeft, curLeftRight), curRight)
       => Node(curLeftVal, curLeftLeft, Node(curVal, curLeftRight, curRight))
     }
   }
+
   override def doubleRightRotate[T >: A : MyOrdering]: Node[T] = {
     this match {
       case Node(curVal, Node(leftVal, leftLeft, Node(leftRightVal, leftRightLeft, leftRightRight)), curRight)
         => Node(leftRightVal, Node(leftVal, leftLeft, leftRightLeft), Node(curVal, leftRightRight, curRight))
     }
   }
+
   override def exists[T >: A : MyOrdering](valueExist: T): Boolean = {
     val comparator = implicitly[MyOrdering[T]]
 
@@ -130,10 +248,6 @@ case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]
       right.exists(valueExist)
   }
 
-/**
-  * @param valueToRemove wartośc do usunięcia
-  * @return Aktualny węzeł (drzewo) po usunięciu wartości
-  */
   override def remove[T >: A : MyOrdering](valueToRemove: T): WBTreeSet[T] = {
     val comparator = implicitly[MyOrdering[T]]
 
@@ -165,6 +279,7 @@ case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]
     else
       Node(value, left, right.remove(valueToRemove)).rebalance
   }
+
   override def remove_and_get_min[T >: A : MyOrdering]:  (T, WBTreeSet[T]) = {
     left match {
       case WBTreeSetNil => (value, right)
@@ -197,6 +312,7 @@ case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]
     else
       inner(WBTreeSetNil, thisIterator.next(), otherIterator.next())
   }
+
   override def sum[T >: A : MyOrdering](other: WBTreeSet[T]): WBTreeSet[T] = {
     val otherIterator = other.iterator
     @tailrec
@@ -213,11 +329,19 @@ case class Node[A: MyOrdering](value: A, left: WBTreeSet[A], right: WBTreeSet[A]
 }
 
 // iteratory w scali są mutowalne
-// iterator chodzi w porządku rosnącym
+/**
+  * WBTreeIterator which goes in ascending order.
+  * @param node
+  * @tparam T type of data stored in nodes
+  */
 private class WBTreeIterator[T](node: Node[T]) extends Iterator[T] {
   val stack = mutable.ArrayStack[Node[T]]()
   goFarLeft(node)
 
+  /**
+    * Go far left from current node.
+    * @param curNode node from which fo far left
+    */
   def goFarLeft(curNode: WBTreeSet[T]): Unit = {
     curNode match {
       case WBTreeSetNil => Unit
@@ -228,8 +352,16 @@ private class WBTreeIterator[T](node: Node[T]) extends Iterator[T] {
     }
   }
 
+  /**
+    * Check if WBTreeSet has next node.
+    * @return true if WBTree has next node otherwise false
+    */
   override def hasNext: Boolean = stack.nonEmpty
 
+  /**
+    * Get next node from WBTreeSet.
+    * @return Next node from WBTreeSet
+    */
   override def next(): T = {
     val current = stack.pop()
     goFarLeft(current.right)
